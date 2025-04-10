97a72eb77bf0ffef9368211070da138912ffa0ba
Oscar Romero <creaturenex@gmail.com>
2025-04-10 13:56:39 -0400

    fixed Card type and added key

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -3,12 +3,19 @@ export default async function Cards() {
     "https://api.scryfall.com/cards/search?include_extras=true&include_variations=true&order=set&q=e:one&unique=prints"
   );
   const cards = await data.json();
-  console.log(cards[0]);
+
+  interface Card {
+    name: string;
+    image_uris: {
+      normal: string;
+    };
+  }
 
   return (
     <div className="flex flex-wrap items-center justify-center min-h-screen p-4 gap-4 ">
-      {cards.data.map((card: any) => (
+      {cards.data.map((card: Card, index: number ) => (
         <img
+          key={index}
           src={card.image_uris.normal}
           alt={card.name}
           className="w-[2.5in] h-[3.5in] rounded-lg bg-black"
